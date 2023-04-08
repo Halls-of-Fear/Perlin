062a763fe2d2399ffdfdcf171f102635be954369
Halls-of-Fear <35615086+Halls-of-Fear@users.noreply.github.com>
2023-04-08 17:04:54 +0300

    ss

--- js.js
+++ js.js
@@ -1,18 +1,24 @@
 const container = document.getElementById("pixelated");
 
 for (let i = 0; i < 8192; i++) {
-  const pixel = document.createElement("div");
-  pixel.classList.add("pixel");
-  container.appendChild(pixel);
+const pixel = document.createElement("div");
+pixel.classList.add("pixel");
+container.appendChild(pixel);
 }
 
 function animateNoise() {
-  const pixels = document.querySelectorAll(".pixel");
+const pixels = document.querySelectorAll(".pixel");
 
-  pixels.forEach(pixel => {
-    const random = Math.floor(Math.random() * 2);
-    pixel.style.backgroundColor = random ? "black" : "white";
-  });
+pixels.forEach(pixel => {
+const random = Math.floor(Math.random() * 20);
+if (random < 8) {
+pixel.style.backgroundColor = "black";
+} else if (random < 16) {
+pixel.style.backgroundColor = "white";
+} else {
+pixel.style.backgroundColor = "black";
+}
+});
 }
 
 setInterval(animateNoise, 150);
