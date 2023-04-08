3842f3cfaf84b1bd33e35e6a3c9385383ead27a3
Halls-of-Fear <35615086+Halls-of-Fear@users.noreply.github.com>
2023-04-08 16:03:23 +0300

    correct and worked version

--- js.js
+++ js.js
@@ -1,24 +1,18 @@
-<script src="https://rawgit.com/josephg/noisejs/master/perlin.js"></script>
-// Get the canvas element
-var canvas = document.getElementById("perlin-canvas");
-var ctx = canvas.getContext("2d");
+const container = document.getElementById("pixelated");
 
-// Generate perlin noise data
-var noise = new Noise(Math.random());
-var data = new Uint8ClampedArray(240 * 320 * 4);
-for (var x = 0; x < 240; x++) {
-  for (var y = 0; y < 320; y++) {
-    var value = noise.perlin2(x / 240, y / 320) * 128 + 128;
-    var index = (x + y * 240) * 4;
-    data[index] = value;
-    data[index + 1] = value;
-    data[index + 2] = value;
-    data[index + 3] = 255;
-  }
+for (let i = 0; i < 8192; i++) {
+  const pixel = document.createElement("div");
+  pixel.classList.add("pixel");
+  container.appendChild(pixel);
 }
 
-// Create a new ImageData object with the data array
-var imageData = new ImageData(data, 240, 320);
+function animateNoise() {
+  const pixels = document.querySelectorAll(".pixel");
 
-// Draw the ImageData on the canvas
-ctx.putImageData(imageData, 0, 0);
+  pixels.forEach(pixel => {
+    const random = Math.floor(Math.random() * 2);
+    pixel.style.backgroundColor = random ? "black" : "white";
+  });
+}
+
+setInterval(animateNoise, 100);
