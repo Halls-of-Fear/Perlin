1395775d24c4f3c07933af71a0ee53e6f5280038
Halls-of-Fear <35615086+Halls-of-Fear@users.noreply.github.com>
2023-04-08 17:17:46 +0300

    sddd

--- js.js
+++ js.js
@@ -6,39 +6,54 @@ pixel.classList.add("pixel");
 container.appendChild(pixel);
 }
 
+const BLACK_FADEOUT_RATE = 0.5;
+
 function animateNoise() {
 const pixels = document.querySelectorAll(".pixel");
-const subgrids = [];
+const blackPixels = [];
 
-// Divide the grid into 16 sub-grids (8x8 pixels each)
-for (let i = 0; i < 16; i++) {
-const subgrid = [];
-for (let j = 0; j < 64; j++) {
-for (let k = 0; k < 8; k++) {
-subgrid.push(pixels[(i * 8 + k) * 64 + j]);
+// Randomly select a few pixels to fill with black pixels
+while (blackPixels.length < 50) {
+const randomIndex = Math.floor(Math.random() * 8192);
+if (blackPixels.indexOf(randomIndex) === -1) {
+blackPixels.push(randomIndex);
 }
-}
-subgrids.push(subgrid);
 }
 
-// Randomly select a few sub-grids to fill with black pixels
-const blackSubgrids = [];
-while (blackSubgrids.length < 5) {
-const randomSubgrid = subgrids[Math.floor(Math.random() * 16)];
-if (blackSubgrids.indexOf(randomSubgrid) === -1) {
-randomSubgrid.forEach(pixel => {
+// Fill the surrounding pixels of each black pixel with black pixels too
+blackPixels.forEach(index => {
+const pixel = pixels[index];
+if (pixel.style.backgroundColor !== "black") {
 pixel.style.backgroundColor = "black";
-});
-blackSubgrids.push(randomSubgrid);
-}
+pixel.fadeout = 1;
 }
 
-// Fill the remaining sub-grids with white pixels
-subgrids.forEach(subgrid => {
-if (blackSubgrids.indexOf(subgrid) === -1) {
-subgrid.forEach(pixel => {
-pixel.style.backgroundColor = "white";
+const x = index % 128;
+const y = Math.floor(index / 128);
+
+for (let i = -1; i <= 1; i++) {
+  for (let j = -1; j <= 1; j++) {
+    if (i === 0 && j === 0) continue;
+    const neighborX = x + i;
+    const neighborY = y + j;
+    if (neighborX >= 0 && neighborX < 128 && neighborY >= 0 && neighborY < 64) {
+      const neighborPixel = pixels[neighborY * 128 + neighborX];
+      if (neighborPixel.style.backgroundColor !== "black") {
+        neighborPixel.style.backgroundColor = "black";
+        neighborPixel.fadeout = 1;
+      }
+    }
+  }
+}
 });
+
+// Fade out black pixels over time
+pixels.forEach(pixel => {
+if (pixel.style.backgroundColor === "black") {
+pixel.fadeout = Math.max(0, pixel.fadeout - BLACK_FADEOUT_RATE);
+pixel.style.opacity = pixel.fadeout;
+} else {
+pixel.fadeout = 0;
 }
 });
 }
